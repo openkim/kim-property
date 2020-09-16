0917f6393f7f80630a17b14c0f1aaa879074bf60
Yaser Afshar <ya.afshar@gmail.com>
2020-09-16 09:21:35 -0500

    update the test and seperate pickle from unpickle test

--- tests/test_kim_property/test_pickle.py
+++ tests/test_kim_property/test_pickle.py
@@ -15,7 +15,7 @@ class TestPickle:
     """Test KIM properties pickle module."""
 
     def test_pickle_kim_properties(self):
-        """Test pickling KIM properties."""
+        """Test pickling the KIM properties."""
         # Property definition edn file
         edn_file1 = join("tests", "fixtures", "atomic-mass.edn")
         self.assertTrue(isfile(edn_file1))
@@ -46,13 +46,47 @@ class TestPickle:
         self.assertRaises(self.KIMPropertyError, pickle_kim_properties,
                           properties, sio)
 
+    def test_unpickle_kim_properties(self):
+        """Test unpickling the KIM properties."""
+
+        # Fails when can't load pickle from unicode string
+        self.assertRaises(self.KIMPropertyError,
+                          unpickle_kim_properties,
+                          'kim_properties.pickle')
+
+        # Property definition edn file
+        edn_file1 = join("tests", "fixtures", "atomic-mass.edn")
+        self.assertTrue(isfile(edn_file1))
+
+        edn_file2 = join("tests", "fixtures",
+                         "cohesive-energy-relation-cubic-crystal.edn")
+        self.assertTrue(isfile(edn_file2))
+
+        properties = {"tag:brunnels@noreply.openkim.org,2016-05-11:property/atomic-mass": kim_edn.load(edn_file1),
+                      "tag:staff@noreply.openkim.org,2014-04-15:property/cohesive-energy-relation-cubic-crystal": kim_edn.load(edn_file2)}
+
+        bio = BytesIO()
+        pickle_kim_properties(properties, bio)
+
         # Test the unpickle utility
         _properties, _name_to_id, _id_to_name = unpickle_kim_properties(
             bio.getvalue())
 
+        name_to_id = {"atomic-mass": "tag:brunnels@noreply.openkim.org,2016-05-11:property/atomic-mass",
+                      "cohesive-energy-relation-cubic-crystal": "tag:staff@noreply.openkim.org,2014-04-15:property/cohesive-energy-relation-cubic-crystal"}
+
+        id_to_name = {"tag:brunnels@noreply.openkim.org,2016-05-11:property/atomic-mass": "atomic-mass",
+                      "tag:staff@noreply.openkim.org,2014-04-15:property/cohesive-energy-relation-cubic-crystal": "cohesive-energy-relation-cubic-crystal"}
+
         self.assertTrue(_properties == properties)
         self.assertTrue(_name_to_id == name_to_id)
         self.assertTrue(_id_to_name == id_to_name)
+
+        bio1 = BytesIO()
+        # Fails when neither can open nor load the input
+        self.assertRaises(self.KIMPropertyError,
+                          unpickle_kim_properties,
+                          bio1.getvalue())
 
 
 class TestPyTestPickle(TestPickle, PyTest):
