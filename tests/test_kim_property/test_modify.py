472f293545e4016c6d8d857ad8a0dde85695c5b6
Yaser Afshar <ya.afshar@gmail.com>
2020-09-17 10:52:45 -0500

    Update the modify test, including tests for issue#3 and extra tests for better coverage

--- tests/test_kim_property/test_modify.py
+++ tests/test_kim_property/test_modify.py
@@ -153,7 +153,7 @@ class TestModifyModule:
                           self.kim_property.kim_property_modify, str_obj,
                           "id")
 
-        # Fails when one forgts to put the comma between inputs
+        # Fails when one forgets to put the comma between inputs
         self.assertRaises(self.KIMPropertyError,
                           self.kim_property.kim_property_modify,
                           str_obj, 1,
@@ -308,9 +308,257 @@ class TestModifyModule:
                           str_obj, 1,
                           "key", "a", "digits", 3.14)
 
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify,
+                          str_obj, 1,
+                          "key", "a", "digits", 1, 5)
+
+        self.assertRaises(ValueError,
+                          self.kim_property.kim_property_modify,
+                          str_obj, 1,
+                          "key", "cohesive-potential-energy", "source-value", "True")
+
+        self.assertRaises(ValueError,
+                          self.kim_property.kim_property_modify,
+                          str_obj, 1,
+                          "key", "cohesive-potential-energy", "source-value", "false")
+
+        self.assertRaises(ValueError,
+                          self.kim_property.kim_property_modify,
+                          str_obj, 1,
+                          "key", "cohesive-potential-energy", "digits", "false")
+
+        self.assertRaises(ValueError,
+                          self.kim_property.kim_property_modify,
+                          str_obj, 1,
+                          "key", "cohesive-potential-energy", "digits", "true")
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify,
+                          str_obj, 1,
+                          "key", "cohesive-potential-energy", "digits", 3.14)
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify,
+                          str_obj, 1,
+                          "key", "cohesive-potential-energy", "digits", 1, 5)
+
+        # Create the property instance with the property name
+        str_obj = self.kim_property.kim_property_create(
+            1, 'cohesive-energy-relation-cubic-crystal')
+
+        # Extra values
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "a", "source-unit", "angstrom",
+                          "digits", "1:5", 5, 5, 5, 5, 5, 5)
+
+        # test digits can be both scalar and array with extent
+
+        # scalar
+        str_obj = self.kim_property.kim_property_modify(
+            str_obj, 1,
+            "key", "a",
+            "source-unit", "angstrom",
+            "digits", 5)
+
+        kim_obj = kim_edn.load(str_obj)
+        self.assertTrue(kim_obj[0]["a"]["digits"] == 5)
+
+        # array with extent
+        str_obj = self.kim_property.kim_property_modify(
+            str_obj, 1,
+            "key", "a",
+            "digits", 5, 5)
+
+        kim_obj = kim_edn.load(str_obj)
+        self.assertTrue(kim_obj[0]["a"]["digits"][0] == 5)
+        self.assertTrue(kim_obj[0]["a"]["digits"][1] == 0)
+        self.assertTrue(kim_obj[0]["a"]["digits"][2] == 0)
+        self.assertTrue(kim_obj[0]["a"]["digits"][3] == 0)
+        self.assertTrue(kim_obj[0]["a"]["digits"][4] == 5)
+
+        str_obj = self.kim_property.kim_property_modify(
+            str_obj, 1,
+            "key", "a",
+            "digits", 1, 5,
+            "digits", 2, 5,
+            "digits", 3, 5,
+            "digits", 4, 5,
+            "digits", 5, 5)
+
+        kim_obj = kim_edn.load(str_obj)
+        self.assertTrue(kim_obj[0]["a"]["digits"][0] == 5)
+        self.assertTrue(kim_obj[0]["a"]["digits"][1] == 5)
+        self.assertTrue(kim_obj[0]["a"]["digits"][2] == 5)
+        self.assertTrue(kim_obj[0]["a"]["digits"][3] == 5)
+        self.assertTrue(kim_obj[0]["a"]["digits"][4] == 5)
+
+        str_obj = self.kim_property.kim_property_modify(
+            str_obj, 1,
+            "key", "a",
+            "digits", "1:5", 5, 5, 5, 5, 5)
+
+        kim_obj = kim_edn.load(str_obj)
+        self.assertTrue(kim_obj[0]["a"]["digits"][0] == 5)
+        self.assertTrue(kim_obj[0]["a"]["digits"][1] == 5)
+        self.assertTrue(kim_obj[0]["a"]["digits"][2] == 5)
+        self.assertTrue(kim_obj[0]["a"]["digits"][3] == 5)
+        self.assertTrue(kim_obj[0]["a"]["digits"][4] == 5)
+
+        # scalar again
+        str_obj = self.kim_property.kim_property_modify(
+            str_obj, 1,
+            "key", "a",
+            "digits", 5)
+
+        kim_obj = kim_edn.load(str_obj)
+        self.assertTrue(kim_obj[0]["a"]["digits"] == 5)
+
+        str_obj = self.kim_property.kim_property_modify(
+            str_obj, 1,
+            "key", "a",
+            "digits", "1:5", 5, 5, 5, 5, 5)
+
+        # Fails when not enough input
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "a",
+                          "digits")
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "a",
+                          "digits", "1:5")
+
+        # Fails when not enough input
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "basis-atom-coordinates",
+                          "source-value")
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "basis-atom-coordinates",
+                          "source-value", 1)
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "basis-atom-coordinates",
+                          "source-value", 1, 1)
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "basis-atom-coordinates",
+                          "source-value", "1:2", "1:2", 0.5, 0.5, 0.5, 0.5)
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "basis-atom-coordinates",
+                          "source-value", "1::2", "1", 0.5, 0.5)
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "basis-atom-coordinates",
+                          "source-value", "1", "1::2", 0.5, 0.5)
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "basis-atom-coordinates",
+                          "source-value", "1", "2:1", 0.5, 0.5)
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "basis-atom-coordinates",
+                          "source-value", "2:1", "1", 0.5, 0.5)
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "basis-atom-coordinates",
+                          "source-value", "1", "4", 0.5)
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "basis-atom-coordinates",
+                          "source-value", "1:2", "4", 0.5, 0.5)
+
+        # Fails when the key has no unit
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "basis-atom-coordinates",
+                          "source-unit", "eV")
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "space-group",
+                          "source-unit", "eV")
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "wyckoff-species",
+                          "source-unit", "eV")
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "wyckoff-multiplicity-and-letter",
+                          "source-unit", "eV")
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "wyckoff-coordinates",
+                          "source-unit", "eV")
+
+        # Create the property instance with the property name
+        str_obj = self.kim_property.kim_property_create(
+            1, 'enthalpy-of-mixing-curve-substitutional-binary-cubic-crystal-npt')
+
+        self.kim_property.kim_property_modify(
+            str_obj, 1, "key", "crystal-is-stable", "source-value", "1", False)
+
+        self.kim_property.kim_property_modify(
+            str_obj, 1, "key", "crystal-is-stable", "source-value", "1:3", True, True, True)
+
+        # Fails when not enough input
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "crystal-is-stable",
+                          "source-value")
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "crystal-is-stable",
+                          "source-value", "1:3")
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "crystal-is-stable",
+                          "source-value", "1:3", True, True)
+
+        self.kim_property.kim_property_modify(
+            str_obj, 1, "key", "short-name", "source-value", "1", "Al")
+
+        self.kim_property.kim_property_modify(
+            str_obj, 1, "key", "short-name", "source-value", "1:3", "Al", "Al", "Al")
+
+        # Fails when not enough input
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "short-name",
+                          "source-value")
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "short-name",
+                          "source-value", "1:3")
+
+        self.assertRaises(self.KIMPropertyError,
+                          self.kim_property.kim_property_modify, str_obj, 1,
+                          "key", "short-name",
+                          "source-value", "1:3", "Al", "Al")
 
     # Extra check for the scalar values
     # see https://github.com/openkim/kim-property/issues/1
+
     def test_modify_exception_issue1(self):
         """Test the modify functionality on exceptions."""
         # Create the property instance with the property name
@@ -339,20 +587,47 @@ class TestModifyModule:
 
         self.assertRaises(self.KIMPropertyError,
                           self.kim_property.kim_property_modify, str_obj, 1,
-                          "key", "a", "source-unit", "angstrom",
-                          "digits", 1, 5)
+                          "key", "cohesive-potential-energy",
+                          "source-unit", 1, "eV")
 
         self.assertRaises(self.KIMPropertyError,
                           self.kim_property.kim_property_modify, str_obj, 1,
                           "key", "cohesive-potential-energy",
-                          "source-unit", 1, "eV", "digits", 1, "5")
+                          "source-unit", "1", "eV")
 
-        self.assertRaises(self.KIMPropertyError,
-                          self.kim_property.kim_property_modify, str_obj, 1,
-                          "key", "cohesive-potential-energy",
-                          "source-value", "1:5", "3.324", "3.3576",
-                          "3.3600", "3.3550", "3.3260",
-                          "source-unit", 1, "eV", "digits", 1, "5")
+    # Extra check for the scalar values
+    # see https://github.com/openkim/kim-property/issues/3
+
+    def test_modify_exception_issue3(self):
+        """Test the modify functionality on exceptions based on issue 3."""
+        # Create the property instance with the property name
+        str_obj = self.kim_property.kim_property_create(
+            1, 'dislocation-core-energy-cubic-crystal-npt')
+
+        E_core_nonsingular_avg = [-0.1764, 0.3393, 0.64111,
+                                  0.8554, 1.3726, 1.67698, 1.8947, 2.0654, 2.20658]
+        E_core_nonsingular_min = [0.01044, 0.01048, 0.01056,
+                                  0.01066, 0.01136, 0.01250, 0.01404, 0.01595, 0.01815]
+        E_core_nonsingular_max = [0.0127, 0.01277, 0.01286,
+                                  0.01299, 0.01389, 0.01535, 0.0173, 0.0197, 0.02247]
+
+        str_obj = self.kim_property.kim_property_modify(
+            str_obj, 1,
+            "key", "core-energy",
+            "source-value", "1:9", *E_core_nonsingular_avg,
+            "source-asym-std-uncert-neg", "1:9", *E_core_nonsingular_min)
+
+        str_obj = self.kim_property.kim_property_modify(
+            str_obj, 1,
+            "key", "core-energy",
+            "source-asym-std-uncert-pos", "1:9", *E_core_nonsingular_max)
+
+        kim_obj = kim_edn.load(str_obj)
+        for i in range(9):
+            self.assertTrue(kim_obj[0]["core-energy"]
+                            ["source-asym-std-uncert-neg"][i] == E_core_nonsingular_min[i])
+            self.assertTrue(kim_obj[0]["core-energy"]
+                            ["source-asym-std-uncert-pos"][i] == E_core_nonsingular_max[i])
 
 
 class TestPyTestModifyModule(TestModifyModule, PyTest):
