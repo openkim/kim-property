6111b422c3ee2eced6a6b20f576d3126dd3efefd
Yaser Afshar <ya.afshar@gmail.com>
2024-03-23 09:30:41 -0700

    update the tag_prefix

--- kim_property/_version.py
+++ kim_property/_version.py
@@ -51,7 +51,7 @@ def get_config() -> VersioneerConfig:
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "v-"
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "kim_property-"
     cfg.versionfile_source = "kim_property/_version.py"
     cfg.verbose = False
